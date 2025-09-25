3a1d7aa13963c36e514c9d5200d7a14e1ffc050a
skfkgla <narahim.lee@gmail.com>
2025-09-25 14:52:19 +0900

    spring cloud config 설정하기

--- spring-cloud-gateway/com.spring-cloud.eureka.client.product/src/main/java/com/spring_cloud/eureka/client/product/ProductController.java
+++ spring-cloud-gateway/com.spring-cloud.eureka.client.product/src/main/java/com/spring_cloud/eureka/client/product/ProductController.java
@@ -1,17 +1,21 @@
 package com.spring_cloud.eureka.client.product;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class ProductController {
 	@Value("${server.port}")
 	private String port;
 
+	@Value("${message}")
+	private String message;
+
 	@GetMapping("/product")
 	public String getProduct() {
-		return "Product info!!!!! From port : " + port;
+		return "Product info!!!!! From port : " + port + "and message : " + message;
 	}
 }
